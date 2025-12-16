6ad4a949b1f1288b671fecedcf898f91b60c1c73
KevTag <kevdasah@gmail.com>
2025-12-16 23:28:21 +0000

    added sets.py

---
+++ 01. core python/01. data_types/5. sets.py
@@ -0,0 +1,15 @@
+#Unordered and no duplicate
+
+cs_courses = {'History', 'Math', 'Physics', 'CompSci'}
+print(cs_courses)
+
+#Intersection
+#Union
+#Difference
+
+cs_courses = {'History', 'Math', 'Physics', 'CompSci'}
+art_courses = {'History', 'Math', 'Art', 'OWOP'}
+
+print(cs_courses.intersection(art_courses))
+print(cs_courses.union(art_courses))
+print(cs_courses.difference(art_courses))
